aa5785e1c928c5b0cb9f7d056f548913a28f2e4c
Roberth Adam da Silva <roberth.silva@vipleiloes.com.br>
2024-05-30 10:40:53 -0300

    Add project files.

---
+++ Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using RegistroBlog.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroBlog.Models
+{
+    public class PostModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
